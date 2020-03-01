9fc2b20ea7c3c5145844ffdb8c6e793b643d6cbe
Satyaki <satyaki.das1920@gmail.com>
2020-03-01 09:26:04 +0000

    wrote function for insert at back of Linked List

--- CSE 208/src/linked_list.c
+++ CSE 208/src/linked_list.c
@@ -1,8 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct SLLNode
+{
+    int data;
+    struct SLLNode* next;
+} SLLNode;
+
+void print_list(SLLNode* head)
+{   
+    SLLNode* curr = head;
+    
+    printf("[");
+    if(curr != NULL)
+    {
+        printf("%d", curr->data);
+        curr = curr->next;
+    }
+    
+    while(curr != NULL)
+    {
+        printf(", %d", curr->data);
+        curr = curr->next;
+    }
+
+    printf("]");
+}
+
+SLLNode* createSLLNode(int data)
+{
+    SLLNode* new_node = malloc(sizeof(SLLNode));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+SLLNode* insert_back(SLLNode* head, int data)
+{
+    SLLNode* curr = head;
+
+    while(curr != NULL && curr->next != NULL) curr = curr->next;
+    if(curr == NULL) head = createSLLNode(data);
+    else curr->next = createSLLNode(data);
+
+    return head;
+}
+
+void deleteSLLNode(SLLNode* node)
+{
+    free(node);
+}
 
 int main()
 {
-    printf("Linked List Start\n");
+    SLLNode* head = NULL;
+
+    while(1)
+    {
+        int action;
+        scanf("%d", &action);
+        if(action == 0) break;
+        else if(action == 1)
+        {
+            int data;
+            scanf("%d", &data);
+            head = insert_back(head, data);
+        }
+        else if(action == 2)
+        {
+
+        }
+        else if(action == 3)
+        {
+            print_list(head);
+            printf("\n");
+        }
+    }
 
     return 0;
 }
